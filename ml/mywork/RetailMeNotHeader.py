801c4a517f97982eb4624bfc912735946dc81624
superbhimanshu <superbhimanshu@gmail.com>
2014-03-31 15:55:31 -0700

    Header file
    For all imports

---
+++ ml/mywork/RetailMeNotHeader.py
@@ -0,0 +1,22 @@
+######### ALL IMPORTS GO HERE ##########
+import os
+os.system('CLS') #for clearing buffer
+import string
+import re
+import operator
+import matplotlib.pyplot as plot
+import nltk
+from nltk.probability import FreqDist
+from gensim import corpora, models, similarities
+from itertools import chain
+from nltk.corpus import stopwords
+from operator import itemgetter
+import random
+import collections
+import nltk.metrics
+import nltk
+from stemming.porter2 import stem
+from gensim import corpora, models, similarities
+from operator import itemgetter
+from itertools import chain
+from nltk.corpus import wordnet as wn
